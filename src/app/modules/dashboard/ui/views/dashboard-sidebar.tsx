f6aa1c5764613964e0de907dddd99a279c03c470
Jawahiir Nabhan <jawahiir.dcastalia@gmail.com>
2025-06-07 15:59:39 +0600

    Dashboard Sidebar layout. Dashboard sidebar initial desgin.

---
+++ src/app/modules/dashboard/ui/views/dashboard-sidebar.tsx
@@ -0,0 +1,57 @@
+'use client';
+import {
+  Sidebar,
+  SidebarTrigger,
+  SidebarFooter,
+  SidebarGroupContent,
+  SidebarGroup,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarHeader,
+  SidebarContent,
+} from '@/components/ui/sidebar';
+import { BotIcon, StarIcon, VideoIcon } from 'lucide-react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { Separator } from '@/components/ui/separator';
+
+const firstSection = [
+  { icon: VideoIcon, label: 'Meetings', href: '/meetings' },
+  { icon: BotIcon, label: 'Agents', href: '/agents' },
+];
+const secondSection = [{ icon: StarIcon, label: 'Upgrade', href: '/upgrade' }];
+
+export const DashboardSidebar = () => {
+  return (
+    <Sidebar>
+      <SidebarHeader className={'text-sidebar-accent-foreground'}>
+        <Link href={'/'} className={'flex items-center gap-2 mt-2 px-2'}>
+          <Image src={'/logo.svg'} height={36} width={36} alt={'KONVO.AI'} />
+          <p className={'text-2xl font-semibold'}>Konvo.AI</p>
+        </Link>
+      </SidebarHeader>
+      <div className="px-4 py-2">
+        <Separator className={'opacity-10 text-[#5D6B68]'} />
+      </div>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu>
+            {firstSection.map((item) => (
+              <SidebarMenuItem key={item.href}>
+                <SidebarMenuButton>
+                  <Link href={item.href} className={'flex'}>
+                    <item.icon className={'size-5'} />
+                    <span className={'text-sm font-medium tracking-tight'}>
+                      {item.label}
+                    </span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+};
