47eca159997295da6ec2220525b2806bb27b6872
Jawahiir Nabhan <jawahiir.dcastalia@gmail.com>
2025-06-08 00:45:52 +0600

    search bar ui design

--- src/app/modules/dashboard/ui/views/dashboard-navbar.tsx
+++ src/app/modules/dashboard/ui/views/dashboard-navbar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { PanelLeftCloseIcon, PanelLeftIcon } from 'lucide-react';
+import { PanelLeftCloseIcon, PanelLeftIcon, SearchIcon } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { useSidebar } from '@/components/ui/sidebar';
 
@@ -16,6 +16,24 @@ export const DashboardNavbar = () => {
         ) : (
           <PanelLeftCloseIcon className={'size-4'} />
         )}
+      </Button>
+      <Button
+        className={
+          'h-9 w-[260px] justify-start font-normal text-muted-foreground hover:text-muted-foreground'
+        }
+        variant={'outline'}
+        size={'sm'}
+        onClick={() => {}}
+      >
+        <SearchIcon /> Search
+        <kbd
+          className={
+            'ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted' +
+            ' px-1.5 font-mono text-[10px] font-medium text-muted-foreground'
+          }
+        >
+          <span className={'text-xs'}>&#8984;</span>K
+        </kbd>
       </Button>
     </nav>
   );
