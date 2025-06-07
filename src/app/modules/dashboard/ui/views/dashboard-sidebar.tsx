f42c8127ca8cf02f9afebffe65c631169246c436
Jawahiir Nabhan <jawahiir.dcastalia@gmail.com>
2025-06-07 23:56:06 +0600

    Dashboard sidebar, placeholder avatar generator, theme colors added to sign in and sign up views, sidebar design

--- src/app/modules/dashboard/ui/views/dashboard-sidebar.tsx
+++ src/app/modules/dashboard/ui/views/dashboard-sidebar.tsx
@@ -1,9 +1,9 @@
 'use client';
+
+import { cn } from '@/lib/utils';
 import {
   Sidebar,
-  SidebarTrigger,
   SidebarFooter,
-  SidebarGroupContent,
   SidebarGroup,
   SidebarMenu,
   SidebarMenuButton,
@@ -15,6 +15,8 @@ import { BotIcon, StarIcon, VideoIcon } from 'lucide-react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { Separator } from '@/components/ui/separator';
+import { usePathname } from 'next/navigation';
+import { DashboardUserButton } from '@/app/modules/dashboard/ui/views/dashboard-user-button';
 
 const firstSection = [
   { icon: VideoIcon, label: 'Meetings', href: '/meetings' },
@@ -23,6 +25,10 @@ const firstSection = [
 const secondSection = [{ icon: StarIcon, label: 'Upgrade', href: '/upgrade' }];
 
 export const DashboardSidebar = () => {
+  const pathname = usePathname();
+  const isPathActive = (currentPath: string) => {
+    return pathname === currentPath;
+  };
   return (
     <Sidebar>
       <SidebarHeader className={'text-sidebar-accent-foreground'}>
@@ -39,7 +45,42 @@ export const DashboardSidebar = () => {
           <SidebarMenu>
             {firstSection.map((item) => (
               <SidebarMenuItem key={item.href}>
-                <SidebarMenuButton>
+                <SidebarMenuButton
+                  asChild
+                  className={cn(
+                    'h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50',
+                    isPathActive(item.href) &&
+                      'bg-linear-to-r/oklch border-[#5D6B68]/10'
+                  )}
+                  isActive={isPathActive(item.href)}
+                >
+                  <Link href={item.href} className={'flex'}>
+                    <item.icon className={'size-5'} />
+                    <span className={'text-sm font-medium tracking-tight'}>
+                      {item.label}
+                    </span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+        <div className="px-4 py-2">
+          <Separator className={'opacity-10 text-[#5D6B68]'} />
+        </div>
+        <SidebarGroup>
+          <SidebarMenu>
+            {secondSection.map((item) => (
+              <SidebarMenuItem key={item.href}>
+                <SidebarMenuButton
+                  asChild
+                  className={cn(
+                    'h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50',
+                    isPathActive(item.href) &&
+                      'bg-linear-to-r/oklch border-[#5D6B68]/10'
+                  )}
+                  isActive={isPathActive(item.href)}
+                >
                   <Link href={item.href} className={'flex'}>
                     <item.icon className={'size-5'} />
                     <span className={'text-sm font-medium tracking-tight'}>
@@ -52,6 +93,9 @@ export const DashboardSidebar = () => {
           </SidebarMenu>
         </SidebarGroup>
       </SidebarContent>
+      <SidebarFooter className={'text-white'}>
+        <DashboardUserButton />
+      </SidebarFooter>
     </Sidebar>
   );
 };
