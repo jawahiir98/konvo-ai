c58a0b5ebeb35e8e8e4a9595d973009bd30baa22
Jawahiir Nabhan <jawahiir.dcastalia@gmail.com>
2025-06-09 20:48:25 +0600

    Proceducre created to handle: Get Single Agent

--- src/app/modules/agents/server/procedures.ts
+++ src/app/modules/agents/server/procedures.ts
@@ -3,12 +3,23 @@ import {
   baseProcedure,
   protectedProcedure,
 } from '@/trpc/init';
+import { z } from 'zod';
 import { db } from '@/db';
 import { agents } from '@/db/schema';
 import { agentsInsertSchema } from './schema';
+import { eq } from 'drizzle-orm';
 
 export const agentsRouter = createTRPCRouter({
-  // TODO: Change 'getMany' to use protectedProcedure
+  // TODO: Change 'getOne' to use protectedProcedure
+  getOne: baseProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input }) => {
+      const [existingAgent] = await db
+        .select()
+        .from(agents)
+        .where(eq(agents.id, input.id));
+      return existingAgent;
+    }),
   getMany: baseProcedure.query(async () => {
     const data = await db.select().from(agents);
     return data;
