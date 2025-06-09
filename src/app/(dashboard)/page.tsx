db00629153a13618d5d0eada3b631b11d8e04e7f
Jawahiir Nabhan <jawahiir.dcastalia@gmail.com>
2025-06-09 16:26:38 +0600

    Created protected procedure and a create prodecure with abstract schema for agent creation inputs.

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -3,6 +3,8 @@ import { redirect } from 'next/navigation';
 import { headers } from 'next/headers';
 import { auth } from '@/lib/auth';
 
+// This component is rendered server side. Component HomeView => Client side rendering
+// So that user redirection logic execute before component mount. Faster navigation.
 const Page = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
--- src/app/modules/agents/server/procedures.ts
+++ src/app/modules/agents/server/procedures.ts
@@ -1,10 +1,28 @@
-import { createTRPCRouter, baseProcedure } from '@/trpc/init';
+import {
+  createTRPCRouter,
+  baseProcedure,
+  protectedProcedure,
+} from '@/trpc/init';
 import { db } from '@/db';
 import { agents } from '@/db/schema';
+import { agentsInsertSchema } from './schema';
 
 export const agentsRouter = createTRPCRouter({
+  // TODO: Change 'getMany' to use protectedProcedure
   getMany: baseProcedure.query(async () => {
     const data = await db.select().from(agents);
     return data;
   }),
+  create: protectedProcedure
+    .input(agentsInsertSchema)
+    .mutation(async ({ input, ctx }) => {
+      const [createdAgent] = await db
+        .insert(agents)
+        .values({
+          ...input,
+          userId: ctx.auth.user.id,
+        })
+        .returning();
+      return createdAgent;
+    }),
 });
