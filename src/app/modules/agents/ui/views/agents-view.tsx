1f51f6896669b4badbadc02a5c2df8e58f17eebd
Jawahiir Nabhan <jawahiir.dcastalia@gmail.com>
2025-06-10 02:17:11 +0600

    Created a data table with empty state component for agents view.

--- src/app/modules/agents/ui/views/agents-view.tsx
+++ src/app/modules/agents/ui/views/agents-view.tsx
@@ -2,10 +2,24 @@
 
 import { useTRPC } from '@/trpc/client';
 import { useSuspenseQuery } from '@tanstack/react-query';
+import { DataTable } from '../components/data-table';
+import { columns } from '../components/columns';
+import { EmptyState } from '@/app/modules/agents/ui/components/empty-table-state';
 
 export const AgentsView = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
-  console.log(data);
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return (
+    <div className={'flex-1 pb-4 px-2 md:px-8 flex flex-col gap-y-4'}>
+      <DataTable data={data} columns={columns} />
+      {data.length === 0 && (
+        <EmptyState
+          title={'Create your first agent'}
+          description={
+            'You can create your first agent by clicking the button below'
+          }
+        />
+      )}
+    </div>
+  );
 };
