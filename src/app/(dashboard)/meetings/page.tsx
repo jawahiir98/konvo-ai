d9efb276166d807335bbe0ed9d07d540b9e32367
Jawahiir Nabhan <jawahiir.dcastalia@gmail.com>
2025-06-12 03:10:05 +0600

    Meetings view boundaries, prefetch added.

--- src/app/(dashboard)/meetings/page.tsx
+++ src/app/(dashboard)/meetings/page.tsx
@@ -1,8 +1,24 @@
+import { Suspense } from 'react';
+import {
+  MeetingsView,
+  MeetingsViewLoading,
+  MeetingsViewError,
+} from '@/app/modules/meetings/ui/views/meetings-view';
+import { getQueryClient, trpc } from '@/trpc/server';
+import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
+import { ErrorBoundary } from 'react-error-boundary';
+
 const Page = () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.meetings.getMany.queryOptions({}));
   return (
-    <div>
-      <div className="">Meeting page</div>
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<MeetingsViewLoading />}>
+        <ErrorBoundary fallback={<MeetingsViewError />}>
+          <MeetingsView />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrationBoundary>
   );
 };
 
