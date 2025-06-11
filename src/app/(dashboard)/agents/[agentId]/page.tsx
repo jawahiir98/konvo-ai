1dd5eaab3f8ddbde5dd1328026f2ff4cfb2a7419
Jawahiir Nabhan <jawahiir.dcastalia@gmail.com>
2025-06-12 01:10:00 +0600

    Single agent view page UI design

--- src/app/(dashboard)/agents/[agentId]/page.tsx
+++ src/app/(dashboard)/agents/[agentId]/page.tsx
@@ -3,6 +3,8 @@ import { ErrorBoundary } from 'react-error-boundary';
 import { getQueryClient, trpc } from '@/trpc/server';
 import { AgentIdView } from '@/app/modules/agents/ui/views/agent-id-view';
 import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
+import { LoadingState } from '@/components/loading-state';
+import { ErrorState } from '@/components/error-state';
 
 interface Props {
   params: Promise<{ agentId: string }>;
@@ -16,8 +18,8 @@ const Page = async ({ params }: Props) => {
   );
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<p>Loading</p>}>
-        <ErrorBoundary fallback={<p>Error</p>}>
+      <Suspense fallback={<AgentsViewLoading />}>
+        <ErrorBoundary fallback={<AgentsError />}>
           <AgentIdView agentId={agentId} />
         </ErrorBoundary>
       </Suspense>
@@ -25,3 +27,18 @@ const Page = async ({ params }: Props) => {
   );
 };
 export default Page;
+
+const AgentsViewLoading = () => {
+  return (
+    <LoadingState
+      title={'Loading Agent'}
+      description={'This may take a few seconds.'}
+    />
+  );
+};
+
+const AgentsError = () => {
+  return (
+    <ErrorState title={'Error'} description={'Failed to load the agent'} />
+  );
+};
---
+++ src/app/modules/agents/ui/views/agent-id-view-header.tsx
@@ -0,0 +1,74 @@
+import {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbSeparator,
+  BreadcrumbLink,
+} from '@/components/ui/breadcrumb';
+import {
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenu,
+} from '@/components/ui/dropdown-menu';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import {
+  ChevronRightIcon,
+  MoreVerticalIcon,
+  TrashIcon,
+  PencilIcon,
+} from 'lucide-react';
+
+interface Props {
+  agentId: string;
+  agentName: string;
+  onEdit: () => void;
+  onRemove: () => void;
+}
+
+export const AgentIdViewHeader = ({
+  agentId,
+  agentName,
+  onEdit,
+  onRemove,
+}: Props) => {
+  return (
+    <div className="flex items-center justify-between">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild className={'font-medium text-xl'}>
+              <Link href={'/agents'}>My Agents</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator
+            className={'text-xl text-foreground font-medium [&>svg]:size-4'}
+          >
+            <ChevronRightIcon />
+          </BreadcrumbSeparator>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild className={'font-medium text-xl'}>
+              <Link href={`/agents/${agentId}`}>{agentName}</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>
+          <Button variant={'ghost'}>
+            <MoreVerticalIcon />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align={'end'}>
+          <DropdownMenuItem onClick={onEdit}>
+            <PencilIcon className={'size-4 text-black'} /> Edit
+          </DropdownMenuItem>
+          <DropdownMenuItem className={'text-destructive'} onClick={onRemove}>
+            <TrashIcon className={'size-4 text-destructive'} /> Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
