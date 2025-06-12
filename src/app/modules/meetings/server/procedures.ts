dad01c74798f87b927a1878b3c4b4908789a70d2
Jawahiir Nabhan <jawahiir.dcastalia@gmail.com>
2025-06-12 23:55:47 +0600

    Modify meetings => getMany procedure to have status and agentId as well.

--- src/app/modules/meetings/server/procedures.ts
+++ src/app/modules/meetings/server/procedures.ts
@@ -10,6 +10,7 @@ import {
 } from '@/constants';
 import { eq, and, getTableColumns, ilike, desc, count, sql } from 'drizzle-orm';
 import { TRPCError } from '@trpc/server';
+import { MeetingStatus } from '@/app/modules/meetings/types';
 import { meetingsInsertSchema } from '@/app/modules/meetings/server/schema';
 import { meetingsUpdateSchema } from '@/app/modules/meetings/server/schema';
 
@@ -42,10 +43,20 @@ export const meetingsRouter = createTRPCRouter({
           .max(MAX_PAGE_SIZE)
           .default(DEFAULT_PAGE_SIZE),
         search: z.string().nullish(),
+        agentId: z.string().nullish(),
+        status: z
+          .enum([
+            MeetingStatus.Active,
+            MeetingStatus.Cancelled,
+            MeetingStatus.Completed,
+            MeetingStatus.Processing,
+            MeetingStatus.Upcoming,
+          ])
+          .nullish(),
       })
     )
     .query(async ({ ctx, input }) => {
-      const { search, page, pageSize } = input;
+      const { search, page, pageSize, status, agentId } = input;
       const data = await db
         .select({
           ...getTableColumns(meetings),
@@ -59,7 +70,9 @@ export const meetingsRouter = createTRPCRouter({
         .where(
           and(
             eq(meetings.userId, ctx.auth.user.id),
-            search ? ilike(meetings.name, `%${search}%`) : undefined
+            search ? ilike(meetings.name, `%${search}%`) : undefined,
+            status ? eq(meetings.status, status) : undefined,
+            agentId ? eq(meetings.agentId, agentId) : undefined
           )
         )
         .orderBy(desc(meetings.createdAt), desc(meetings.id))
@@ -74,7 +87,9 @@ export const meetingsRouter = createTRPCRouter({
         .where(
           and(
             eq(meetings.userId, ctx.auth.user.id),
-            search ? ilike(meetings.name, `%${search}%`) : undefined
+            search ? ilike(meetings.name, `%${search}%`) : undefined,
+            status ? eq(meetings.status, status) : undefined,
+            agentId ? eq(meetings.agentId, agentId) : undefined
           )
         );
       const totalPages = Math.ceil(total.count / pageSize);
